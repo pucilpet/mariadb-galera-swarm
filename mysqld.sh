89affbfc623e6f920217912cff7e56880f058325
Colin Mollenhour <colin@mollenhour.com>
2016-09-29 22:47:36 -0400

    Rebase Dockerfile on mariadb:10.1 and add support for Docker Swarm Mode.

--- mysqld.sh
+++ mysqld.sh
@@ -9,6 +9,7 @@ MYSQLD=mysqld
 LOG_MESSAGE="Docker startscript: "
 wsrep_recover_position=
 OPT="$@"
+START=""
 
 # Do we want to check for programms?
 
@@ -19,15 +20,14 @@ which $MYSQLD || exit 1
 
 test -d /var/lib/mysql/mysql
 if [ $? != 0 ]; then
-  mysql_install_db --user=mysql
+  mysql_install_db
   if [ $? != 0 ]; then
     echo "${LOG_MESSAGE} Tried to install mysql.* schema because /var/lib/mysql seemed empty"
     echo "${LOG_MESSAGE} it failed :("
   fi
 fi
 
-# Get the GTID possition
-
+# Get the GTID position
 echo  "${LOG_MESSAGE} Get the GTID positon"
 tmpfile=$(mktemp)
 $MYSQLD --wsrep-recover 2>${tmpfile}
@@ -43,7 +43,9 @@ wsrep_start_position=$(sed -n 's/.*Recovered\ position:\s*//p' ${tmpfile})
 # What should we do if there is no recoverd position?
 # We will not start, as most likely Galera is not configured
 
-if test -z ${wsrep_start_position}
+if test -n ${wsrep_start_position}
+  then START="--wsrep_start_position=$wsrep_start_position"
+elif ! [[ "$OPT" =~ /--wsrep-new-cluster/ ]]
   then echo "${LOG_MESSAGE} We found no wsrep position!"
        echo "${LOG_MESSAGE} Most likely Galera is not configured, so we refuse to start"
        exit 1
@@ -51,7 +53,8 @@ fi
 
 # Start mysqld
 
-exec $MYSQLD $OPT --wsrep_start_position=$wsrep_start_position
+echo  "${LOG_MESSAGE} Starting with options: $OPT"
+exec $MYSQLD $OPT $START
 
 # We should never end in here
 
